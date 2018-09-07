9d9d4da537a8e9b407e8e607a37b9c839807158d
wxq393 <wangxiaoqin.0009@163.com>
2018-09-07 23:18:34 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <h1>nihao, world!</h1>,
+    document.getElementById('root2')
+  );

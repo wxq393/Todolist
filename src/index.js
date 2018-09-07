2039dec60801f59d490c74873f1283cbd765171b
wxq393 <wangxiaoqin.0009@163.com>
2018-09-07 23:57:25 +0800

    基本骨架完成

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
 ReactDOM.render(
-    <h1>nihao, world!</h1>,
+    <App />,
     document.getElementById('root2')
   );

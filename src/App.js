e31a1050d436abc6af163f521dfa69e80148eaf1
wxq393 <wangxiaoqin.0009@163.com>
2018-09-09 21:55:12 +0800

    仿照注册，完成登录功能

--- src/App.js
+++ src/App.js
@@ -42,8 +42,11 @@ render() {
           <ol className="todoList">
              {todos}
          </ol>
-         {this.state.user.id ? null : <UserDialog onSignUp={this.onSignUp.bind(this)}/>}
-      
+         {this.state.user.id ? 
+         null : 
+         <UserDialog 
+            onSignUp={this.onSignUp.bind(this)} 
+            onSignIn={this.onSignIn.bind(this)}/>}
       </div>
     )
   }
@@ -53,7 +56,12 @@ render() {
     stateCopy.user = {} 
     this.setState(stateCopy)
   }
-componentDidUpdate(){
+  onSignIn(user){
+    let stateCopy = JSON.parse(JSON.stringify(this.state)) 
+    stateCopy.user = user
+    this.setState(stateCopy)
+  }
+  componentDidUpdate(){
   }
   toggle(e, todo){
     todo.status = todo.status === 'completed' ? '' : 'completed'
--- src/leanCloud.js
+++ src/leanCloud.js
@@ -27,7 +27,16 @@ export function signUp(username, password, successFn, errorFn){
     return undefined
 
   }
-
+  
+  export function signIn(username, password, successFn, errorFn){
+    AV.User.logIn(username, password).then(function (loginedUser) {
+      let user = getUserFromAVUser(loginedUser)
+      successFn.call(null, user)
+    }, function (error) {
+      errorFn.call(null, error)
+    })
+  }
+  
   export function getCurrentUser(){
     let user = AV.User.current()
     if(user){
@@ -40,7 +49,7 @@ export function signUp(username, password, successFn, errorFn){
     AV.User.logOut()
     return undefined
   }
-  
+
   function getUserFromAVUser(AVUser){
    return {
      id: AVUser.id,

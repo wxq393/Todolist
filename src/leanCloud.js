3ef76b4f88dcf8cfbf01329fa822839a0434c9e3
wxq393 <wangxiaoqin.0009@163.com>
2018-09-10 00:39:21 +0800

    添加返回登录按钮

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -56,10 +56,10 @@ export function signUp(email, username, password, successFn, errorFn){
     AV.User.requestPasswordReset(email).then(function (success) {
       successFn.call() 
     }, function (error) {
-      console.dir(error)
+     errorFn.call(null, error)
     })
   }
-  
+
   function getUserFromAVUser(AVUser){
    return {
      id: AVUser.id,

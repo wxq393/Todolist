872fb6076d27b492ea25915663dcac88e3cd58da
wxq393 <wangxiaoqin.0009@163.com>
2018-09-10 00:32:30 +0800

    用户点击发送时，请求 LeanCloud 的 API

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -18,7 +18,7 @@ export function signUp(email, username, password, successFn, errorFn){
    user.setPassword(password)
    // 设置邮箱
    user.setEmail(email)
-   
+
    user.signUp().then(function (loginedUser) {
      let user = getUserFromAVUser(loginedUser)
      successFn.call(null, user)
@@ -51,7 +51,15 @@ export function signUp(email, username, password, successFn, errorFn){
     AV.User.logOut()
     return undefined
   }
-
+  
+  export function sendPasswordResetEmail(email, successFn, errorFn){
+    AV.User.requestPasswordReset(email).then(function (success) {
+      successFn.call() 
+    }, function (error) {
+      console.dir(error)
+    })
+  }
+  
   function getUserFromAVUser(AVUser){
    return {
      id: AVUser.id,

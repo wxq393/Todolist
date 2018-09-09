d78a643e33121d87f59bbcd1ff3e778637a97020
wxq393 <wangxiaoqin.0009@163.com>
2018-09-09 14:36:01 +0800

    新建 leanCloud.js

---
+++ src/leanCloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'Yt4sMYkDKDzaROfviWSFIjua-gzGzoHsz'
+var APP_KEY = 'PxpSXagGMyIkVvMUTFbXK602'
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
+export default AV

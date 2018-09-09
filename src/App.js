9d7a6d0e1de60b9e7de9f6c51a71119afd01225e
wxq393 <wangxiaoqin.0009@163.com>
2018-09-09 12:09:47 +0800

    删除 todo

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ class App extends Component {
     }
   }
   render() {
-
     let todos = this.state.todoList.map((item,index)=>{
       return ( // 为什么这里要加个括号？这是动手题3 🐸
         <li key={index} >
-           <TodoItem todo={item} onToggle={this.toggle.bind(this)} />
+            <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+            onDelete={this.delete.bind(this)}/> 
         </li>
       )
     })
@@ -60,6 +60,10 @@ class App extends Component {
       newTodo: '',
       todoList: this.state.todoList
     })
+  }
+  delete(event, todo){
+    todo.deleted = true
+    this.setState(this.state) 
   }
 }
 

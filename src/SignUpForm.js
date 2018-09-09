93a4c7c97ff22ee8cf7141a5264389bfb757233b
wxq393 <wangxiaoqin.0009@163.com>
2018-09-10 01:28:32 +0800

    更新 SignUpForm 改成一个函数

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -8,9 +8,9 @@ export default function (props) {
             </div>
             <div className="row">
                 <label>用户名</label>
-                <input type="text" value=
-                onChange={props.onChange.bind(null, 'username')}/>
-                {/* bind 不仅可以绑定 this，还可以绑定第一个参数 */}
+                <input type="text" value= {props.formData.username}
+                    onChange ={props.onChange.bind (null, 'username')}/>
+                   {/* bind 不仅可以绑定 this，还可以绑定第一个参数 */}
             </div>
             <div className="row">
                 <label>密码</label>

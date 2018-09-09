6c6c0dcb707107ce4107b5bbc9abb1391ffb96fb
wxq393 <wangxiaoqin.0009@163.com>
2018-09-09 23:08:49 +0800

    注册时需要提供邮箱，以便找回密码

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -7,6 +7,7 @@ export default class UserDialog extends Component{
         this.state = {
           selected: 'signUp',
           formData: {
+            email: '',
             username: '',
             password: '',
           }
@@ -19,7 +20,7 @@ export default class UserDialog extends Component{
       }
     signUp(e){
         e.preventDefault()
-    let {username, password} = this.state.formData
+    let {email, username, password} = this.state.formData
     let success = (user)=>{
         this.props.onSignUp.call(null, user)
     }
@@ -33,8 +34,8 @@ export default class UserDialog extends Component{
           break
       }
     }
-    signUp(username, password, success, error)
-    }  
+    signUp(email, username, password, success, error)
+}	}
     signIn(e) {
         e.preventDefault()
     let {username, password} = this.state.formData
@@ -62,58 +63,63 @@ export default class UserDialog extends Component{
         let signUpForm = (
             <form className="signUp" onSubmit={this.signUp.bind(this)}> {/* 注册*/}
                 <div className="row">
-                <label>用户名</label> 
-                <input type="text" value={this.state.formData.username}
-                onChange={this.changeFormData.bind(this, 'username')}/>
-                 {/* bind 不仅可以绑定 this，还可以绑定第一个参数 */}
+                    <label>邮箱</label> 
+                    <input type="text" value={this.state.formData.email}
+                        onChange={this.changeFormData.bind(this, 'email')}/>
                 </div>
                 <div className="row">
-                <label>密码</label>
-                <input type="password" value={this.state.formData.password} 
-                onChange={this.changeFormData.bind(this, 'password')}/>
+                    <label>用户名</label> 
+                    <input type="text" value={this.state.formData.username}
+                    onChange={this.changeFormData.bind(this, 'username')}/>
+                    {/* bind 不仅可以绑定 this，还可以绑定第一个参数 */}
+                </div>
+                <div className="row">
+                    <label>密码</label>
+                    <input type="password" value={this.state.formData.password} 
+                    onChange={this.changeFormData.bind(this, 'password')}/>
                 </div>
                 <div className="row actions">
-                <button type="submit">注册</button>
+                    <button type="submit">注册</button>
                 </div>
             </form>
       )
       let signInForm = (
-        <form className="signIn" onSubmit={this.signIn.bind(this)}> {/* 登录*/}
-            <div className="row">
-                <label>用户名</label>
-                <input type="text" value={this.state.formData.username}
-                onChange={this.changeFormData.bind(this, 'username')}/>
-            </div>
-            <div className="row">
-                <label>密码</label>
-                <input type="password" value={this.state.formData.password}
-                onChange={this.changeFormData.bind(this, 'password')}/>
-            </div>
-            <div className="row actions">
-                <button type="submit">登录</button>
-                <a href="javascript:;">忘记密码了？</a>
-            </div>
-        </form>
+            <form className="signIn" onSubmit={this.signIn.bind(this)}> {/* 登录*/}
+                <div className="row">
+                    <label>用户名</label>
+                    <input type="text" value={this.state.formData.username}
+                    onChange={this.changeFormData.bind(this, 'username')}/>
+                </div>
+                <div className="row">
+                    <label>密码</label>
+                    <input type="password" value={this.state.formData.password}
+                    onChange={this.changeFormData.bind(this, 'password')}/>
+                </div>
+                <div className="row actions">
+                    <button type="submit">登录</button>
+                    <a href="javascript:;">忘记密码了？</a>
+                </div>
+            </form>
       ) 
     return (
       <div className="UserDialog-Wrapper">
         <div className="UserDialog">
-        <nav>
-          <label>
-          <input type="radio" value="signUp"
-          checked={this.state.selected === 'signUp'} 
-          onChange={this.switch.bind(this)}
-          /> 注册</label>
-            <label>
-            <input type="radio" value="signIn"
-            checked={this.state.selected === 'signIn'} 
-            onChange={this.switch.bind(this)}
-            /> 登录</label>
-        </nav>
-        <div className="panes">
-        {this.state.selected === 'signUp' ? signUpForm : null}
-        {this.state.selected === 'signIn' ? signInForm : null}
-        </div>
+            <nav>
+                <label>
+                <input type="radio" value="signUp"
+                checked={this.state.selected === 'signUp'} 
+                onChange={this.switch.bind(this)}
+                /> 注册</label>
+                <label>
+                    <input type="radio" value="signIn"
+                    checked={this.state.selected === 'signIn'} 
+                    onChange={this.switch.bind(this)}
+                /> 登录</label>
+            </nav>
+            <div className="panes">
+                {this.state.selected === 'signUp' ? signUpForm : null}
+                {this.state.selected === 'signIn' ? signInForm : null}
+            </div>
         </div>
       </div>
     )
--- src/leanCloud.js
+++ src/leanCloud.js
@@ -9,7 +9,7 @@ AV.init({
 
 export default AV
 
-export function signUp(username, password, successFn, errorFn){
+export function signUp(email, username, password, successFn, errorFn){
     // 新建 AVUser 对象实例
    var user = new AV.User()
    // 设置用户名
@@ -17,6 +17,8 @@ export function signUp(username, password, successFn, errorFn){
    // 设置密码
    user.setPassword(password)
    // 设置邮箱
+   user.setEmail(email)
+   
    user.signUp().then(function (loginedUser) {
      let user = getUserFromAVUser(loginedUser)
      successFn.call(null, user)
@@ -36,7 +38,7 @@ export function signUp(username, password, successFn, errorFn){
       errorFn.call(null, error)
     })
   }
-  
+
   export function getCurrentUser(){
     let user = AV.User.current()
     if(user){
